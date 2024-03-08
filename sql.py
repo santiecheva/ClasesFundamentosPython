34c4be94f5e16ef7072663df15e4c7bd6721b45e
Santiago <santiecheva@gmail.com>
2024-03-08 18:34:21 -0500

    read function

--- sql.py
+++ sql.py
@@ -8,6 +8,14 @@ conn_params = {
     }
 
 def get_connection(conn_params):
+    """Esta función me genera una conexión para alguna base de datos postgres dada
+
+    Args:
+        conn_params (dict): parametros de conexion, con host, usuario y contraseña
+
+    Returns:
+        conn: Conexión
+    """
     try:
         conn = psycopg2.connect(**conn_params)
         return conn
@@ -17,41 +25,18 @@ def get_connection(conn_params):
 
 conn = get_connection(conn_params)
 cur = conn.cursor()
-cur.execute("SELECT title, description FROM film LIMIT 10;")
-print(cur.fetchall())
 
-
-
-
-
-
-def get_film_details():
-    # Connection parameters
-    conn_params = {
-        'dbname': 'dvdrental',
-        'user': 'santiago', # or your PostgreSQL username
-        'password': 'cofifi73', # replace with your password
-        'host': 'localhost'
-    }
-    
+def read(query: str,cur):
     try:
-        # Connect to the PostgreSQL database
-        conn = psycopg2.connect(**conn_params)
-        
-        # Create a cursor object
-        cur = conn.cursor()
-        
-        # Execute a query
-        cur.execute("SELECT title, description FROM film LIMIT 10;")
-        
-        # Fetch and print the results
-        for row in cur.fetchall():
-            print(f"Title: {row[0]}, Description: {row[1]}")
-            
-        # Close the cursor and connection
-        cur.close()
-        conn.close()
-        
-    except psycopg2.Error as e:
-        print(f"Error: {e}")
-        
+        cur.execute(query)
+        return cur.fetchall()
+    except:
+        print("No se pudo ejecutar el query")
+
+query = "SELECT title, description FROM film LIMIT 10;"
+
+#data = read(query,cur)
+
+
+#for value in data:
+#    print(f"EL título de la película es {value[0]} y trata de {value[1]}")
